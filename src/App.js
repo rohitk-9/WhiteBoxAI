6a29fa6c43fa70c08fe79235f8d2632178692cf8
rohitk-9 <rohit94639463@gmail.com>
2024-11-16 20:10:31 +0530

    props in navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-      <Navbar /><br/>
+      <Navbar val ="Front-end Editor" /><br/>
       <Landing />
       {/* <Footer /> */}
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import './componentStyle/Navbar.css'
-import Ai2 from "./Ai2"
+// import Ai2 from "./Ai2"
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Sidebar from "./Sidebar"
+// import FrontendEditor from "./FrontendEditor"
 
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
 <>
 <div class="topnav">
   <a class="active" href="#home" ><Sidebar /></a>
-  <a href="#contact"><span id = "nav-txt"> hello</span></a>
-  <a href="#about"><span id = "nav-txt">hello</span></a>
+  <a href="#contact"><span id = "nav-txt"> {props.val}</span></a>
+  {/* <a href="#about"><span id = "nav-txt">hello</span></a> */}
 </div>
-
-
 </>
   )
 }
