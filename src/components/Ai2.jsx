d5cfbac617ab9d131fc2b79bb4404c1c1c7fafcb
rohitk-9 <rohit94639463@gmail.com>
2024-11-16 21:57:00 +0530

    working with key

--- src/components/Ai2.jsx
+++ src/components/Ai2.jsx
@@ -30,7 +30,7 @@ function App() {
     
     try {
       const response = await axios({
-        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=`,
+        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=`+process.env.API_KEY,
         method: "post",
         data: {
           contents: [{ parts: [{ text: question }] }],
