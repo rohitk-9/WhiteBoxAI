3af86af18df556a9755f9b00be584bc33185a3a0
rohitk-9 <rohit94639463@gmail.com>
2024-11-16 21:14:07 +0530

    b4 deploy

--- src/App.js
+++ src/App.js
@@ -14,38 +14,9 @@ function App() {
             <Route path="/components/FrontendEditor"  element={<FrontendEditor/>} />
           </Routes>
       </div>
-      {/* <Landing /> */}
-      {/* <Footer /> */}
+
     </>
   );
 }
 
 export default App;
-
-
-
-/*
-import './App.css';
-import { Route,Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Stocks from './components/stocks';
-import Favourites from './components/favourites';
-import Cart from './components/cart';
-
-function App() {
-  return (
-   <>
-    <Navbar/>
-      <div>
-    <Routes>        
-      <Route path="/"  element={<Stocks/>} />
-      <Route path="/favourites"  element={<Favourites />}/>
-      <Route path="/cart"  element={<Cart/>}/>
-    </Routes>
-    </div>
-    </>
-   
-  );
-}
-
-export default App;*/
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import './componentStyle/Navbar.css'
-// import Ai2 from "./Ai2"
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Sidebar from "./Sidebar"
-// import FrontendEditor from "./FrontendEditor"
 
 
 export default function Navbar(props) {
@@ -19,45 +16,3 @@ export default function Navbar(props) {
 </>
   )
 }
-
-
-
-
-// const Navbar=()=>{
-//     return (    
-//                 <div className='navBar'>
-//                     <div className='stocks'>
-//                         <Link to="/">Stocks</Link>
-//                     </div>
-//                     <div className='FrontendEditor'>
-//                         <Link to="/components/FrontendEditor">FrontendEditor</Link>
-//                     </div>
-//                 </div>
-//     )
-
-// }
-
-// export default Navbar;
-
-
-
-
-/*
-import React from 'react'
-import './componentStyle/Navbar.css'
-// import Ai2 from "./Ai2"
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Sidebar from "./Sidebar"
-// import FrontendEditor from "./FrontendEditor"
-
-
-export default function Navbar(props) {
-  return (
-<>
-<div class="topnav">
-  <a class="active" href="#home" ><Sidebar /></a>
-  <a href="#contact"><span id = "nav-txt"> {props.val}</span></a>
-</div>
-</>
-  )
-} */
